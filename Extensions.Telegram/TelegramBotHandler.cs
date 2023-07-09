736e7d52e1bc2634676f3908041805b354c46722
boajr <piero.boato@gmail.com>
2023-07-09 11:37:01 +0200

    Removed unused using

--- Extensions.Telegram/TelegramBotHandler.cs
+++ Extensions.Telegram/TelegramBotHandler.cs
@@ -6,7 +6,6 @@ using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Boa.Identity.Telegram
 {
